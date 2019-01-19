c689ecf5c58f6c592cb160f61fcc78ba8bddce01
AlvaroDS <alvaroenrique020@gmail.com>
2019-01-18 19:08:42 -0500

    se avanza con los productos

---
+++ src/com/lacoraza/bean/Talla.java
@@ -0,0 +1,22 @@
+package com.lacoraza.bean;
+
+public class Talla {
+    int idTalla;
+    String talla;
+
+    public int getIdTalla() {
+        return idTalla;
+    }
+
+    public void setIdTalla(int idTalla) {
+        this.idTalla = idTalla;
+    }
+
+    public String getTalla() {
+        return talla;
+    }
+
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+}
